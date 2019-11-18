4deb7eca01cd07c1a9b7b411e97037da9f9feb55
Richard Torhan <richard.torhan@windowslive.com>
2019-11-18 14:31:28 +0100

    Conversion to .NET Core

--- IdeaRS.OpenModel/Properties/AssemblyInfo.cs
+++ IdeaRS.OpenModel/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IdeaStatiCa.OpenModel")]
+//[assembly: AssemblyTitle("IdeaStatiCa.OpenModel")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
 //[assembly: AssemblyProduct("IdeaRS.OpenModel")]
 //[assembly: AssemblyCopyright("Copyright ©  2015")]
