d8845148cb2846adca649c19d85ab617d39af6ee
Martin Pospisil <martin.pospisil@ideastatica.com>
2020-06-22 13:48:24 +0200

    adding class ApplyConnTemplateSetting

---
+++ IdeaRS.OpenModel/Connection/ApplyConnTempleteSetting.cs
@@ -0,0 +1,29 @@
+﻿namespace IdeaRS.OpenModel.Connection
+{
+	/// <summary>
+	/// Defaults which are used for applying template
+	/// </summary>
+	public class ApplyConnTempleteSetting
+	{
+		/// <summary>
+		/// ID of the bolt assembly which will be used as default (it must exists in the project)
+		/// </summary>
+		public int DefaultBoltAssemblyID { get; set; }
+
+		/// <summary>
+		/// ID of the cross section for cleats which will be used as default (it must exists in the project)
+		/// </summary>
+		public int DefaultCleatCrossSectionID { get; set; }
+
+		/// <summary>
+		/// ID of the material concrete which will be used as default (it must exists in the project)
+		/// </summary>
+		public int DefaultConcreteMaterialID { get; set; }
+
+		/// <summary>
+		/// ID of the cross-section for stiffening members concrete which will be used as default (it must exists in the project)
+		/// </summary>
+
+		public int DefaultStiffMemberCrossSectionID { get; set; }
+	}
+}
